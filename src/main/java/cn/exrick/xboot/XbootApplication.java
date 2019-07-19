2a082248308e3f7023d4996513d4dda8a1a778e1
ilvxna@hotmail.com <plmokn>
2019-07-19 17:26:26 +0800

    first project

---
+++ src/main/java/cn/exrick/xboot/XbootApplication.java
@@ -0,0 +1,13 @@
+package cn.exrick.xboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbootApplication.class, args);
+	}
+
+}
